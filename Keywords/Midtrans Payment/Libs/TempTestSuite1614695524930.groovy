ecf0ffa3fabe76e5933d29997e589b11433d4ad1
malikspy <malikspy@malikspy-X441UA>
2021-03-04 09:02:43 +0700

    Commit Update

---
+++ Keywords/Midtrans Payment/Libs/TempTestSuite1614695524930.groovy
@@ -0,0 +1,39 @@
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.exception.StepFailedException
+import com.kms.katalon.core.reporting.ReportUtil
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.testdata.TestDataColumn
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import internal.GlobalVariable as GlobalVariable
+
+Map<String, String> suiteProperties = new HashMap<String, String>();
+
+
+suiteProperties.put('id', 'Test Suites/Midtrans/Test_Payment')
+
+suiteProperties.put('name', 'Test_Payment')
+
+suiteProperties.put('description', '')
+
+suiteProperties.put('stopImmediately', 'false')
+ 
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.testng.keyword.internal.TestNGDriverCleaner())
+
+
+
+RunConfiguration.setExecutionSettingFile("/home/malikspy/Katalon Studio/Midtrans Payment/Reports/20210302_213204/Midtrans/Test_Payment/20210302_213204/execution.properties")
+
+TestCaseMain.beforeStart()
+
+TestCaseMain.startTestSuite('Test Suites/Midtrans/Test_Payment', suiteProperties, new File("/home/malikspy/Katalon Studio/Midtrans Payment/Reports/20210302_213204/Midtrans/Test_Payment/20210302_213204/testCaseBinding"))
